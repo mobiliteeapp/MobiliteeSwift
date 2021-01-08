80a1bca06ffda3dd38ad34d66a77fdb8900272e6
Jero Sanchez <jero.sanchez@icloud.com>
2021-01-08 12:02:19 +0100

    Extract `NearestBusStop` to its own file

---
+++ MobiliteeSwift/Nearest Bus Stops Feature/NearestBusStop.swift
@@ -0,0 +1,21 @@
+//
+//  Copyright (c) 2021 Jero Sánchez. All rights reserved.
+//
+
+import Foundation
+
+public struct NearestBusStop {
+    public let id: Int
+    public let latitude: Double
+    public let longitude: Double
+    public let name: String
+    public let address: String
+    public let distance: Int
+    public let lines: [NearestBusStopLine]
+}
+
+public struct NearestBusStopLine {
+    public let id: Int
+    public let origin: String
+    public let destination: String
+}
--- MobiliteeSwift/Nearest Bus Stops Feature/NearestBusStopsLoader.swift
+++ MobiliteeSwift/Nearest Bus Stops Feature/NearestBusStopsLoader.swift
@@ -4,22 +4,6 @@
 
 import Foundation
 
-public struct BusStop {
-    public let id: Int
-    public let latitude: Double
-    public let longitude: Double
-    public let name: String
-    public let address: String
-    public let distance: Int
-    public let lines: [BusStopLine]
-}
-
-public struct BusStopLine {
-    public let id: Int
-    public let origin: String
-    public let destination: String
-}
-
 public protocol NearestBusStopsLoader {
-    func load(latitude: Double, longitude: Double, radius: Int, completion: (Result<[BusStop], Error>))
+    func load(latitude: Double, longitude: Double, radius: Int, completion: (Result<[NearestBusStop], Error>))
 }
