e09faf398d6cf6433c8548e1f0bb10c02948105f
Jero Sanchez <jero.sanchez@icloud.com>
2021-01-11 13:35:54 +0100

    Extract `RemoteNearestBusStop` type to its own file

--- MobiliteeSwift/Nearest Bus Stops API/NearestBusStopsMapper.swift
+++ MobiliteeSwift/Nearest Bus Stops API/NearestBusStopsMapper.swift
@@ -31,38 +31,3 @@ final class NearestBusStopsMapper {
         }
     }
 }
-
-struct RemoteNearestBusStop: Decodable {
-    let stopId: Int
-    let geometry: Geometry
-    let stopName: String
-    let address: String
-    let metersToPoint: Int
-    let lines: [RemoteNearestBusStopLine]
-    
-    var nearestBusStop: NearestBusStop {
-        let nearestBusStopline = lines.map { $0.nearestBusStopline }
-        return NearestBusStop(
-            id: stopId,
-            latitude: geometry.coordinates[1],
-            longitude: geometry.coordinates[0],
-            name: stopName,
-            address: address,
-            distance: metersToPoint,
-            lines: nearestBusStopline)
-    }
-}
-
-struct Geometry: Decodable {
-    let coordinates: [Double]
-}
-
-struct RemoteNearestBusStopLine: Decodable {
-    let line: String
-    let nameA: String
-    let nameB: String
-    
-    var nearestBusStopline: NearestBusStopLine {
-        return NearestBusStopLine(id: line, origin: nameA, destination: nameB)
-    }
-}
